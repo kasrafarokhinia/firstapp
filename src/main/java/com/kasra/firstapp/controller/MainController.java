ab232cb422d7307d6b474141fdf70957719d5b28
kasrafarokhinia <webdeveloperkasra@gmail.com>
2023-02-06 18:05:44 +0330

    Initial commit

---
+++ src/main/java/com/kasra/firstapp/controller/MainController.java
@@ -0,0 +1,40 @@
+package com.kasra.firstapp.controller;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+public class MainController {
+
+    @RequestMapping(value = "/path/{username}/{password}", method = RequestMethod.POST)
+    public String pathToString(@PathVariable("username") String username,@PathVariable("password") String password){
+        return username + ',' + password;
+    }
+    @RequestMapping(value = "/param", method = RequestMethod.POST)
+    public String param(@RequestParam("username") String username){
+        return username ;
+    }
+
+    @RequestMapping(value = "/body", method = RequestMethod.POST)
+    public String body(@RequestBody String username){
+        return username ;
+    }
+
+    @RequestMapping(value = "/",method = RequestMethod.POST)
+    public String changeToString() {
+        return "kasraaaaaaaaaaaaa";
+    }
+    @RequestMapping(value = "/",method = RequestMethod.GET)
+    public Integer changeToNumebr() {
+        return 125654;
+    }
+    @GetMapping(value = "/get")
+    public String getString(){
+        return "omad";
+    }
+    @PostMapping(value = "/post")
+    public String postString()
+    {
+        return "omaaaaaaaaaaaaaaaaaad";
+    }
+
+}
---
+++ src/test/java/com/kasra/firstapp/FirstappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kasra.firstapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
